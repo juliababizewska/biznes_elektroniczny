824badf95b3de507b52cc892f87401a33e7c0741
root <root@LAPTOP-6958SL39.>
2024-11-07 12:20:28 +0100

    Source files and config files added

---
+++ kody-zrodlowe-sklepu/my-prestashop/var/cache/prod/smarty/cache/ps_banner/1/1/1/2/14/classic/b4/34/a4/b434a49c202fd63ec8a1cd736cae30f7fa7c3553.psbannerpsbanner.tpl.php
@@ -0,0 +1,26 @@
+<?php
+/* Smarty version 3.1.48, created on 2024-11-07 11:40:53
+  from 'module:psbannerpsbanner.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.48',
+  'unifunc' => 'content_672c9935327400_03553853',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '64034093fbda864710a2c0f526b37f921e53ee39' => 
+    array (
+      0 => 'module:psbannerpsbanner.tpl',
+      1 => 1702485415,
+      2 => 'module',
+    ),
+  ),
+  'cache_lifetime' => 31536000,
+),true)) {
+function content_672c9935327400_03553853 (Smarty_Internal_Template $_smarty_tpl) {
+?><a class="banner" href="https://localhost:8080/pl/" title="">
+      <img src="https://localhost:8080/modules/ps_banner/img/sale70.png" alt="" title="" class="img-fluid" loading="lazy" width="1110" height="213">
+  </a>
+<?php }
+}
