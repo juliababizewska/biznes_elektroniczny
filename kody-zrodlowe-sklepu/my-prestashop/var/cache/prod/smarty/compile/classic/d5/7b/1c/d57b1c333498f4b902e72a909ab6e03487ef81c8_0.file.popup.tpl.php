8254d18e8ac02fd090b797ae0f8f63fa6325500f
wderwisz <wojtek@Laptop-Legion.>
2024-11-19 18:59:06 +0100

    Email translation test

---
+++ kody-zrodlowe-sklepu/my-prestashop/var/cache/prod/smarty/compile/classic/d5/7b/1c/d57b1c333498f4b902e72a909ab6e03487ef81c8_0.file.popup.tpl.php
@@ -0,0 +1,29 @@
+<?php
+/* Smarty version 3.1.48, created on 2024-11-19 18:31:43
+  from '/var/www/html/modules/welcome/views/templates/popup.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.48',
+  'unifunc' => 'content_673ccb7f837795_27473686',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'd57b1c333498f4b902e72a909ab6e03487ef81c8' => 
+    array (
+      0 => '/var/www/html/modules/welcome/views/templates/popup.tpl',
+      1 => 1730979360,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_673ccb7f837795_27473686 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<div class="onboarding-popup bootstrap">
+  <div class="content"></div>
+</div>
+<?php }
+}
